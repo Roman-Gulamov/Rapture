a3d2218fc7af1e14d5fb10351cff352789634f6b
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-20 19:22:54 +0300

    All files have been moved to separate folders

--- assets/src/js/main.js
+++
@@ -1,8 +0,0 @@
-//= ./jquery-cookie/src/jquery.cookie.js
-//= ./scroll/scrollTo.js
-//= ./scroll/scrollUp.js
-//= ./header/header.js
-//= ./submenu/submenu.js
-//= ./features/features.js
-//= ./components/components.js
-//= ./phone/phone.js
--- assets/src/php/mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-$email = filter_var(trim($_POST['email']), FILTER_SANITIZE_STRING);
-
-    $mysql = new mysqli ('localhost', 'root', '', 'rapture');
-    $mysql->query("SET NAMES 'utf8'");
-    $mysql->query("INSERT INTO `email` (`email`) VALUES('$email')");
-    $mysql->close ();
-?>
